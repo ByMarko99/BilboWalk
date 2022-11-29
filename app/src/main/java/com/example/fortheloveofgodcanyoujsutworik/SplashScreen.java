d1256a76edfb471d09a8c7ae132bd74191f49390
GontzalIzur <gontzal.izurzare@elorrieta-errekamari.com>
2022-11-29 13:16:33 +0100

    SplashScreen implementation

---
+++ app/src/main/java/com/example/fortheloveofgodcanyoujsutworik/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.fortheloveofgodcanyoujsutworik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import java.time.Duration;
+
+public class SplashScreen extends Activity {
+
+    private final int DURATION_SPLASH = 1500; // El delay
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Esta es la orientacion de la pantalla
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // Esto lo que nos va hacer es que la aplicacion se ponga en pantalla completa
+
+        setContentView(R.layout.activity_splash_screen);
+        // Llama a la actividad
+
+        new Handler().postDelayed(new Runnable() {
+            // Esto hace un delay y despues nos manda a la pantalla de login
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DURATION_SPLASH); // Aqui se llama al delay
+    }
+}
