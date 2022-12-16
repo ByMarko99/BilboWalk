70527ad0ab74fd828e3b2aa679f45e2d6b671b3b
ArahejiElor <102594372+ArahejiElor@users.noreply.github.com>
2022-12-16 10:54:16 +0300

    Colores

--- app/src/main/java/com/example/fortheloveofgodcanyoujsutworik/PreguntasTest.java
+++ app/src/main/java/com/example/fortheloveofgodcanyoujsutworik/PreguntasTest.java
@@ -24,7 +24,7 @@ public class PreguntasTest extends AppCompatActivity {
     RadioButton Marijaia;
     RadioButton Begoña;
     RadioButton SantoTomas;
-    Button btnNext2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -98,16 +98,6 @@ public class PreguntasTest extends AppCompatActivity {
                     textView7.setTextColor(getResources().getColor(R.color.red));
 
                 }
-
-                btnNext2 = findViewById(R.id.btnNext2);
-                btnNext2.setOnClickListener(new View.OnClickListener() {
-                    // Nos mandara a una pantalla de registro la cual nos dejara crear un usuario
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(PreguntasTest.this, Preguntas.class);
-                        startActivity(intent);
-                    }
-                });
 
             }
         });
